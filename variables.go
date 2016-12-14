b0ee580fb2fc9e0e567d4f5fb8cea3f1e6711e96
yannick.devos <yannick.devos@qxlricardo.fr>
2016-12-14 09:25:38 +0100

    new test

---
+++ variables.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Package level variable declaration (= global)
+var (
+	name   = "Nigel"            //Name of the subscriber (Type String)
+	course = "Docker Deep Dive" //Couse being viewed (Type String)
+	module = 3.2                //Current position in course (Type float)
+)
+
+func main() {
+	// Function level variable declaration + initialization (= work ONLY inside of the function)
+	// Variables declared at fucntion level MUST be used (error otherwise)
+	//Function level variable overwrite Package level variable
+	name := "Yannick" //Name of the subscriber (Type String)
+	//course := "Docker Deep Dive 2" //Couse being viewed (Type String)
+	module := 3.4  //Current position in course (Type float)
+	ptr := &module //declare pointer value of module directly
+
+	fmt.Println("Name is", name, "and is of type", reflect.TypeOf(name))
+	fmt.Println("Module is", module, "and is of type", reflect.TypeOf(module))
+	fmt.Println("Memory address of *module* variable is", &module) //Print the memory hexadecimal address value of the variable instead of its type = aka pointer value
+	fmt.Println("Memory address of *module* variable is", ptr,     //Do the same as previous using ptr value
+		"and the value of *module* is", *ptr) //Use * to de-reference the pointer => display the variable's value
+}
